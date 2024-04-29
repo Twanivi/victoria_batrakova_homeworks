d95f899fb32312f1b0c69e73b2dac99cf1da5efb
Twanivi <aikitashoo@gmail.com>
2024-04-29 22:15:39 +0300

    add new file

--- homework_15/task3.py
+++ homework_15/task3.py
@@ -10,7 +10,7 @@ import time
 
 
 with open('rows_300.csv', 'w', encoding='utf-8', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
+    writer = csv.writer(file)
     writer.writerow(['№', 'Секунда ', 'Микросекунда'])
     for line in range(1, 301):
         writer.writerow([line, datetime.datetime.now().second, datetime.datetime.now().microsecond])
