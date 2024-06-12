c845b4a5b94f4b841f0ecd9b1afb5eda4faee220
Twanivi <aikitashoo@gmail.com>
2024-06-12 08:59:10 +0300

    add new file

--- homework_19/task2.py
+++ homework_19/task2.py
@@ -15,51 +15,36 @@
 # - Нет ограничений по реализации классов CarMan и Vehicle, а также вспомогательной логики.
 
 class Vehicle:
-    def __init__(self):
-        self.id = 1
+    id = 1
+    @classmethod
+    def counter(cls):
+        cls.id = Vehicle.id
+        Vehicle.id += 1
 
+    def __new__(cls, *args, **kwargs):
+        cls.counter()
+        return super().__new__(cls)
 
 class CarMan(Vehicle):
-    def __init__(self):
-        super().__init__()
-        object_id_collector = self.id
-
-    def change_number(self):
-        if Car():
-            self.id += 1
-        elif CarCommander():
-            self.id += 1
-        elif CarGunner():
-            self.id += 1
-        else:
-            return self.id
-
+    pass
 
 class Car(Vehicle):
     def __init__(self):
-        super().__init__()
         object_id_collector = self.id
 
 
 class CarCommander(Vehicle):
     def __init__(self):
-        super().__init__()
         object_id_collector = self.id
 
 
 class CarGunner(Vehicle):
     def __init__(self):
-        super().__init__()
         object_id_collector = self.id
-
-
-
 
 
 def check_object_id_collector():
     expected_ids = (1, 2, 3, 4, 5)
-    # CarMan().id
-    CarMan.change_number(CarMan().id)
     actual_ids = (CarGunner().id, CarGunner().id, Car().id, CarCommander().id, Car().id)
     assert actual_ids == expected_ids, 'Expected_ids: {}. Actual_ids: {}.'.format(expected_ids, actual_ids)
     print('Test passed. Amazing job!')
